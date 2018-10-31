2e1f6da2c32b08352df6bd9e6a19573f432051c6
Santiago-Rocha <santiago.rocha@tulpep.com>
2018-10-31 10:41:00 -0500

    conexion a la BD

---
+++ src/main/java/edu/eci/pdsw/samples/services/InitiativeServiceFactory.java
@@ -0,0 +1,52 @@
+package edu.eci.pdsw.samples.services;
+
+
+import org.mybatis.guice.XMLMyBatisModule;
+import org.mybatis.guice.datasource.helper.JdbcHelper;
+
+import static com.google.inject.Guice.createInjector;
+import com.google.inject.Injector;
+
+
+public class InitiativeServiceFactory {
+
+    private static InitiativeServiceFactory instance = new InitiativeServiceFactory();
+    
+    private static Injector injector;
+    private static Injector testingInjector;
+    
+    private InitiativeServiceFactory(){
+        injector = createInjector(new XMLMyBatisModule() {
+
+                    @Override
+                    protected void initialize() {
+                        install(JdbcHelper.MySQL);                        
+                        setClassPathResource("mybatis-config.xml");                        
+                    }
+
+                }
+                
+        );
+        
+        testingInjector = createInjector(new XMLMyBatisModule() {
+
+                    @Override
+                    protected void initialize() {
+                        install(JdbcHelper.MySQL);                        
+                        setClassPathResource("h2-mybatis-config.xml");                        
+                        
+                    }
+
+                }
+                
+        );
+        
+    }
+    
+    public static InitiativeServiceFactory getInstance(){
+        return instance;
+    }
+    
+   
+    
+}
