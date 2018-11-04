63b8f9c5149db045b77e01eb2b0dd7530e560149
anamaria1299 <ana.rincon-c@mail.escuelaing.edu.co>
2018-11-04 17:10:41 -0500

    Cambios base de datos

--- tables.sql
+++ tables.sql
@@ -1,17 +1,17 @@
-CREATE TABLE TypeUser (
-   id int  NOT NULL,
-   type_user varchar(40)  NOT NULL,
-   CONSTRAINT TypeUser_uk UNIQUE (type_user) NOT DEFERRABLE  INITIALLY IMMEDIATE,
-   CONSTRAINT TypeUser_pk PRIMARY KEY (id)
-);
-
 CREATE TABLE "User" (
    id int  NOT NULL,
    name varchar(50)  NOT NULL,
    email varchar(100)  NOT NULL,
-   TypeUser_id varchar(40)  NOT NULL,
+   Password varchar(30)  NOT NULL,
+   Type_user int  NOT NULL,
    CONSTRAINT email UNIQUE (email) NOT DEFERRABLE  INITIALLY IMMEDIATE,
-   CONSTRAINT User_pk PRIMARY KEY (id)
+   CONSTRAINT user_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE vote (
+   User_id int  NOT NULL,
+   Initiative_id int  NOT NULL,
+   CONSTRAINT vote_pk PRIMARY KEY (User_id,Initiative_id)
 );
 
 CREATE TABLE Comment (
@@ -22,23 +22,16 @@ CREATE TABLE Comment (
    CONSTRAINT Comment_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE vote (
-   User_id int  NOT NULL,
-   Initiative_id int  NOT NULL,
-   CONSTRAINT vote_pk PRIMARY KEY (User_id,Initiative_id)
-);
-
 CREATE TABLE Initiative (
    id int  NOT NULL,
    description varchar(150)  NOT NULL,
-   key_words varchar(100)  NOT NULL,
    area varchar(100)  NOT NULL,
    num_votos int  NOT NULL,
    creation_date date  NOT NULL,
    User_id int  NOT NULL,
    modify_date int  NOT NULL,
    Type_Status_id varchar(50)  NOT NULL,
-   CONSTRAINT Initiative_pk PRIMARY KEY (id)
+   CONSTRAINT initiative_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE Type_Status (
@@ -48,30 +41,20 @@ CREATE TABLE Type_Status (
    CONSTRAINT Type_Status_pk PRIMARY KEY (id)
 );
 
---Foreing Key
+CREATE TABLE word_initiative (
+   Initiative_id int  NOT NULL,
+   Key_words_id int  NOT NULL,
+   CONSTRAINT word_initiative_pk PRIMARY KEY (Initiative_id,Key_words_id)
+);
 
-ALTER TABLE Initiative ADD CONSTRAINT Initiative_Type_Status
-   FOREIGN KEY (Type_Status_id)
-   REFERENCES Type_Status (status)  
-   NOT DEFERRABLE 
-   INITIALLY IMMEDIATE
-;
+CREATE TABLE Key_words (
+   id int  NOT NULL,
+   word varchar(15)  NOT NULL,
+   CONSTRAINT Key_words_pk PRIMARY KEY (id)
+);
 
-ALTER TABLE Comment ADD CONSTRAINT Comment_Initiative
-   FOREIGN KEY (Initiative_id)
-   REFERENCES Initiative (id)  
-   NOT DEFERRABLE 
-   INITIALLY IMMEDIATE
-;
-
-ALTER TABLE vote ADD CONSTRAINT vote_Initiative
-   FOREIGN KEY (Initiative_id)
-   REFERENCES Initiative (id)  
-   NOT DEFERRABLE 
-   INITIALLY IMMEDIATE
-;
-
-ALTER TABLE Initiative ADD CONSTRAINT Initiative_User
+--Foreign key
+ALTER TABLE Comment ADD CONSTRAINT Comment_User
    FOREIGN KEY (User_id)
    REFERENCES "User" (id)  
    NOT DEFERRABLE 
@@ -85,25 +68,58 @@ ALTER TABLE vote ADD CONSTRAINT vote_User
    INITIALLY IMMEDIATE
 ;
 
-ALTER TABLE Comment ADD CONSTRAINT Comment_User
+ALTER TABLE Initiative ADD CONSTRAINT Initiative_User
    FOREIGN KEY (User_id)
    REFERENCES "User" (id)  
    NOT DEFERRABLE 
    INITIALLY IMMEDIATE
 ;
 
-ALTER TABLE "User" ADD CONSTRAINT User_TypeUser
-   FOREIGN KEY (TypeUser_id)
-   REFERENCES TypeUser (type_user)  
+ALTER TABLE vote ADD CONSTRAINT vote_Initiative
+   FOREIGN KEY (Initiative_id)
+   REFERENCES Initiative (id)  
    NOT DEFERRABLE 
    INITIALLY IMMEDIATE
 ;
---Insert
-INSERT into "typeuser" values (1,'Administrador');
-INSERT into "typeuser" values (2,'Personal');
-INSERT into "typeuser" values (3,'Proponente');
-INSERT into "typeuser" values (4,'Publico');
 
-insert into "User" values (2133,'Ana','anamariarincon1299@gmail.com','Administrador');
-insert into "User" values (2143,'Santiago','santiagorocha@gmail.com','Proponente');
+ALTER TABLE Comment ADD CONSTRAINT Comment_Initiative
+   FOREIGN KEY (Initiative_id)
+   REFERENCES Initiative (id)  
+   NOT DEFERRABLE 
+   INITIALLY IMMEDIATE
+;
+
+ALTER TABLE Initiative ADD CONSTRAINT Initiative_Type_Status
+   FOREIGN KEY (Type_Status_id)
+   REFERENCES Type_Status (status)  
+   NOT DEFERRABLE 
+   INITIALLY IMMEDIATE
+;
+
+ALTER TABLE word_initiative ADD CONSTRAINT word_initiative_Initiative
+   FOREIGN KEY (Initiative_id)
+   REFERENCES Initiative (id)  
+   NOT DEFERRABLE 
+   INITIALLY IMMEDIATE
+;
+
+ALTER TABLE word_initiative ADD CONSTRAINT word_initiative_Key_words
+   FOREIGN KEY (Key_words_id)
+   REFERENCES Key_words (id)  
+   NOT DEFERRABLE 
+   INITIALLY IMMEDIATE
+;
+
+/*
+ * Para los tipo usuario se tienen de la siguiente forma:
+ * 1 - Administrador
+ * 2 - Personal
+ * 3 - Proponente
+ * 4 - Publico
+ * */
+
+insert into "User" values (2133,'Ana','anamariarincon1299@escuelaing.edu.co','ana123',1);
+insert into "User" values (2143,'Santiago','santiago@mail.escuelaing.edu.co','santiago123',3);
+
+
 
