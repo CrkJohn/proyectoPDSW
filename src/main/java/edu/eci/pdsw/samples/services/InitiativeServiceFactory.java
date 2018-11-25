ddfabee90cc3a7b98a201abda1aef4bf05e8e618
k26duran <k26duran@gmail.com>
2018-11-25 12:40:52 -0500

    añadiendo fecha a tablakeyword

--- src/main/java/edu/eci/pdsw/samples/services/InitiativeServiceFactory.java
+++ src/main/java/edu/eci/pdsw/samples/services/InitiativeServiceFactory.java
@@ -103,9 +103,9 @@ public class InitiativeServiceFactory {
     	System.out.println(InitiativeServiceFactory.getInstance().getInitiativeServices().getDataChart());
     	System.out.println(InitiativeServiceFactory.getInstance().getInitiativeServices().listInitiative());*/
     	//InitiativeServiceFactory.getInstance().getInitiativeServices().createInitiative("holi", "hey", 2133, kw, "soy nombre");
-    	System.out.println(InitiativeServiceFactory.getInstance().getInitiativeServices().loadInitiativeByUser(2133));
-    	System.out.println("Consulta iniciativas en estado de: en revision");
-    	System.out.println(InitiativeServiceFactory.getInstance().getInitiativeServices().loadByStatus("En espera revision"));
+    	//System.out.println(InitiativeServiceFactory.getInstance().getInitiativeServices().loadInitiativeByUser(2133));
+    	//System.out.println("Consulta iniciativas en estado de: en revision");
+    	//System.out.println(InitiativeServiceFactory.getInstance().getInitiativeServices().loadByStatus("En espera revision"));
     	
     	
     	     
