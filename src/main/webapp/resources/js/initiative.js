46f92e4d57672553a483f76e25b5705fd761ae10
Santiago-Rocha <santiago.rocha@tulpep.com>
2018-12-03 17:34:32 -0500

    cambios en details

--- src/main/webapp/resources/js/initiative.js
+++ src/main/webapp/resources/js/initiative.js
@@ -46,6 +46,26 @@ $(document).on("click", ".modify-Initiative", function () {
 });
 
 $(document).ready(function() {
+    var table = $('#keyinitiative').DataTable();
+     
+	$('#keyinitiative tbody').on('click', 'td.details', function () {
+	    var tr = $(this).closest('tr');
+	    var sp = $(this).children().eq(1);
+	    var row = table.row(tr);
+	    if ( row.child.isShown() ) {
+			$(this).children().children().first().removeClass("fa-minus-square").addClass("fa-plus-square");
+	        row.child.hide();
+	        tr.removeClass('shown');
+	    }
+	    else {
+	        row.child(sp.html()).show();
+	        tr.addClass('shown');
+			$(this).children().children().first().removeClass("fa-plus-square").addClass("fa-minus-square");
+	    }
+	} );
+} );
+
+$(document).ready(function() {
     var table = $('#initiativeTable').DataTable();
      
 	$('#initiativeTable tbody').on('click', 'td.details', function () {
