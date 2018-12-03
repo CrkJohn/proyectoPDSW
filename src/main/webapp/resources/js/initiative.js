778c74298250bad5a86fee226b2b4e9cc25bad31
Santiago-Rocha <santiago.rocha@tulpep.com>
2018-12-03 13:25:02 -0500

    arreglo descripcion

--- src/main/webapp/resources/js/initiative.js
+++ src/main/webapp/resources/js/initiative.js
@@ -48,17 +48,19 @@ $(document).on("click", ".modify-Initiative", function () {
 $(document).ready(function() {
     var table = $('#initiativeTable').DataTable();
      
-	$('#initiativeTable tbody').on('click', 'td.details-control', function () {
+	$('#initiativeTable tbody').on('click', 'td.details', function () {
 	    var tr = $(this).closest('tr');
-	    var sp = $(this).children().first();
+	    var sp = $(this).children().eq(1);
 	    var row = table.row(tr);
 	    if ( row.child.isShown() ) {
+			$(this).children().children().first().removeClass("fa-minus-square").addClass("fa-plus-square");
 	        row.child.hide();
 	        tr.removeClass('shown');
 	    }
 	    else {
 	        row.child(sp.html()).show();
 	        tr.addClass('shown');
+			$(this).children().children().first().removeClass("fa-plus-square").addClass("fa-minus-square");
 	    }
 	} );
 } );
